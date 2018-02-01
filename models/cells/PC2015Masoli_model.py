5a066eb6b1f3ad45bff2298871e307d04731e074
lungsi <lungsi@users.noreply.github.com>
2018-02-01 14:44:13 +0100

    Added record in reset()

--- models/cells/PC2015Masoli_model.py
+++ models/cells/PC2015Masoli_model.py
@@ -391,6 +391,9 @@ class PurkinjeCell( sciunit.Model,
         self.cell.rec_t = h.Vector()
         self.cell.vm_soma = h.Vector()
         self.cell.axonNOR3 = h.Vector()
+        self.cell.rec_t.record(h._ref_t)
+        self.cell.vm_soma.record(self.soma(0.5)._ref_v)
+        self.cell.vm_NOR3.record(self.axonNOR3(0.5)._ref_v)
         
     # ----Class method----
     # PurkinjeCell()
