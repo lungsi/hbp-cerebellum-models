28f0e34c5ca97fa18d44f79e3b7e21f527c5422e
lungsi <lungsi@users.noreply.github.com>
2018-02-02 12:15:14 +0100

    Update PC2015Masoli_model.py

--- models/cells/PC2015Masoli_model.py
+++ models/cells/PC2015Masoli_model.py
@@ -395,7 +395,7 @@ class PurkinjeCell( sciunit.Model,
         #self.cell.vm_soma.record(self.cell.soma(0.5)._ref_v)
         #self.cell.vm_NOR3.record(self.cell.axonNOR3(0.5)._ref_v)
         self.cell.parallelcontext.gid_clear()
-        self.cell.parallelcontext = h.ParallelContext()
+        #self.cell.parallelcontext = h.ParallelContext()
         self.pid = os.fork()
         print('intialization done')
         
